aa20da2c2474cd49425f7cf0de97a4667a04c1a5
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2024-12-11 00:35:39 +0100

    Refactor ProfilePage component for improved readability and structure

--- modules/client/src/main/scala/org/worldofscala/app/profile/ProfilePage.scala
+++ modules/client/src/main/scala/org/worldofscala/app/profile/ProfilePage.scala
@@ -14,27 +14,26 @@ object ProfilePage:
   val userBus = new EventBus[User]
 
   def apply(): ReactiveHtmlElement[HTMLDivElement] = div(
-    child <-- session:
-      // If the user is not logged in, show a message
-      div(h1("Please log in to view your profile"))
-      // If the user is logged in, show the profile page
-    (_ =>
+    child <-- session(h1("Please log in to view your profile")) { _ =>
       div(
         onMountCallback { _ =>
           UserEndpoint.profile(()).emitTo(userBus)
         },
-        div(
-          h1("Profile Page"),
-          child <-- userBus.events.map { user =>
-            div(
-              cls := "srf-form",
-              h2("User"),
-              div("First name: ", user.firstname),
-              div("Last name: ", user.lastname),
-              div("Email: ", user.email)
-            )
-          }
-        )
+        renderUser
       )
-    )
+    }
   )
+
+  def renderUser =
+    div(
+      h1("Profile Page"),
+      child <-- userBus.events.map { user =>
+        div(
+          cls := "srf-form",
+          h2("User"),
+          div("First name: ", user.firstname),
+          div("Last name: ", user.lastname),
+          div("Email: ", user.email)
+        )
+      }
+    )
