243150b14e2981fb3d53f758e7abf7440727ed05
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2024-12-27 12:37:44 +0100

    Update flywaydb version to 11.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 object Dependencies {
   val Versions = new {
     val chimney               = "1.5.0"
-    val flywaydb              = "11.0.0"
+    val flywaydb              = "11.1.0"
     val frontroute            = "0.19.0"
     val iron                  = "2.6.0"
     val javaMail              = "1.6.2"
