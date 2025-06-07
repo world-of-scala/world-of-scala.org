11ae335db9a5d7f596f2b978f3658a07a1bdebf7
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2025-06-07 16:35:46 +0200

    Refactor HttpServer to simplify run method and remove unused imports

--- modules/server/src/main/scala/org/worldofscala/Main.scala
+++ modules/server/src/main/scala/org/worldofscala/Main.scala
@@ -4,27 +4,17 @@ import zio.*
 import zio.logging.LogFormat
 import zio.logging.backend.SLF4J
 
-import org.worldofscala.config.ServerConfig
 import org.worldofscala.http.Server
-import org.worldofscala.services.*
-import org.worldofscala.repository.Repository
+import org.worldofscala.services.FlywayService
 
 object HttpServer extends ZIOAppDefault:
 
   override val bootstrap = SLF4J.slf4j(LogFormat.colored)
 
-  private def program = for {
-    _ <- FlywayService.runMigrations
-    _ <- Server.start
-  } yield ()
-
   override def run: Task[Unit] =
-    program
-      .provide(
-        ServerConfig.layer,
-        FlywayServiceLive.configuredLayer
-
-        // , ZLayer.Debug.mermaid
-      )
+    for {
+      _ <- FlywayService.runMigrations
+      _ <- Server.start
+    } yield ()
 
 end HttpServer
